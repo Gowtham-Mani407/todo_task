04a92fea0101139872045aed36cc7a5b9a92a39c
Gowtham-Mani407 <gowtham2kid@gmail.com>
2025-09-15 15:31:46 -0700

    BLoC based ToDo app with unit test case

--- test/list_screen_test.dart
+++ test/list_screen_test.dart
@@ -8,7 +8,6 @@ import 'package:todo_task/view/screens/homescreen.dart';
 
 void main() {
   testWidgets("Add task and display in list", (WidgetTester tester) async {
-    // 1. Render HomeScreen inside a BlocProvider
     final taskBloc = TaskBloc();
 
     await tester.pumpWidget(
@@ -18,7 +17,6 @@ void main() {
       ),
     );
 
-    // 2. Dispatch event to add dummy task
     taskBloc.add(
       AddTask(
         Task(
@@ -29,14 +27,11 @@ void main() {
       ),
     );
 
-    // 3. Pump to let UI rebuild with new state
     await tester.pumpAndSettle();
 
-    // 4. Verify the new task is displayed
     expect(find.text("Gowtham today should buy groceries"), findsOneWidget);
     expect(find.text("Milk, eggs, and bread"), findsOneWidget);
 
-    // (Optional) verify that "No tasks yet" message is gone
     expect(find.text("No tasks yet , add some!"), findsNothing);
   });
 }
